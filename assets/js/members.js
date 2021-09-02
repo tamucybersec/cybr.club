b0917580cd3bc54af0db0fbe1947f5aef690bf43
Matthew Le <maattt01@gmail.com>
2021-09-02 13:34:20 -0500

    fixed officer titles

--- assets/js/members.js
+++ assets/js/members.js
@@ -45,7 +45,7 @@ const officers = [
   },
   {
     name: "Adele Walker",
-    position: "Internal Relations Officer",
+    position: "Internal Relations",
     image: "assets/img/officers/formatted/adele-walker.jpg",
     socials: {
         linkedin: "https://www.linkedin.com/in/adele-w-a75ab7ba/",
@@ -56,7 +56,7 @@ const officers = [
   },
   {
     name: "Jacob Kastenschmidt",
-    position: "External Relations Officer",
+    position: "External Relations",
     image: "assets/img/officers/formatted/jacob-kastenschmidt.jpg",
     socials: {
         linkedin: "https://www.linkedin.com/in/jacob-kastenschmidt-b217731a8/",
