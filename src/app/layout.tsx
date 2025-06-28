e63f9a8eb46f79649fe531eb7c8259dca24ed6b1
luvadlamudi <lucasvad123@gmail.com>
2025-06-28 14:33:48 -0500

    next init

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const azonix = localFont({
+  src: "../../public/fonts/Azonix.otf",
+  variable: "--font-azonix",
+  weight: "400",
+  style: "normal",
+});
+
+const ubuntuSans = localFont({
+  src: "../../public/fonts/UbuntuSans-VariableFont_wdth,wght.ttf",
+  variable: "--font-ubuntu-sans",
+  weight: "100 900",
+  style: "normal",
+});
+
+export const metadata: Metadata = {
+  title: "cybr.club",
+  description: "Texas A&M Cybersecurity Club",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${azonix.variable} ${ubuntuSans.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
